2a724e9d161f4b16f9596de18ea63bf4cb1fb397
Nery37 <Viniciusneryb13@hotmail.com>
2020-11-30 19:59:19 -0300

    Refatorando botão

--- resources/views/categoria_produto/index.blade.php
+++ resources/views/categoria_produto/index.blade.php
@@ -4,7 +4,7 @@
     <section class="content-header">
         <h1 class="pull-left">Categoria </h1>
         <h1 class="pull-right">
-           <a class="btn btn-primary pull-right" style="margin-top: -10px;margin-bottom: 5px" href="{{ route('categoriaProduto.create') }}">Add New</a>
+           <a class="btn btn-primary pull-right" style="margin-top: -10px;margin-bottom: 5px" href="{{ route('categoriaProduto.create') }}">Cadastrar</a>
         </h1>
     </section>
     <div class="content">
